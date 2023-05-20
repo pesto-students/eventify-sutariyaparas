e71adef27c537629560c99a1532da3edada094a6
Paras D Sutariya <paras.sutariya125@gmail.com>
2023-05-21 02:52:56 +0530

    Add website frontend skeleton

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">EVENTIFY</div>
+      <div className="contact">Contact Us</div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ frontend/src/pages/Website/FaqPage/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const FaqPage = () => {
+  return (
+    <div>
+      <h1>FAQ</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis est mauris. Nulla vitae ultricies ex.
+        Fusce nec arcu eget tellus feugiat vulputate. In in vestibulum metus. Curabitur dictum justo vitae purus
+        rhoncus, sit amet consectetur risus posuere. Suspendisse accumsan purus id erat blandit, in dignissim enim
+        lacinia. Cras id diam ut orci scelerisque lobortis.
+      </p>
+      <p>
+        Donec a tincidunt velit, eu congue libero. Phasellus sit amet lacus risus. Sed elementum risus et neque
+        fringilla sagittis. Aliquam blandit erat quis nisi cursus, eu finibus mi scelerisque. Integer sollicitudin
+        vulputate mi, ac tristique velit vehicula at. Suspendisse sagittis tellus in lobortis hendrerit.
+      </p>
+    </div>
+  );
+};
+
+export default FaqPage;
